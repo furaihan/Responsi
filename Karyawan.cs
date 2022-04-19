e4a090257e9b6d5a15f8c6f204687f1a4ed0d4a5
furaihan <28698437+furaihan@users.noreply.github.com>
2022-04-19 11:34:53 +0700

    initial commit

---
+++ Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4374
+{
+    internal class Karyawan
+    {
+        public Karyawan(string nik, int gaji, string nama)
+        {
+            this.NIK = nik;
+            this.GajiBulanan = gaji > 0 ? gaji : 0;
+            this.Nama = nama;
+        }
+        public string NIK { get; set; }
+        public int GajiBulanan { get; set; }
+        public string Nama { get; set; }
+        public void AddSalaryPercentage(int percentage)
+        {
+            if (percentage > 0)
+            {
+                GajiBulanan += (GajiBulanan * percentage / 100);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ResponsiPemrograman4374
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("1903024455", 2500000, "Virgil Van Dijk");
+            Karyawan karyawan2 = new Karyawan("1903024798", 3500000, "Sergio Ramos");
+            Console.WriteLine("No  NIK/Nama \t\t\t\t Gaji Bulanan");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine($"1.  {karyawan1.NIK} {karyawan1.Nama} \t\t {karyawan1.GajiBulanan}");
+            Console.WriteLine($"1.  {karyawan2.NIK} {karyawan2.Nama} \t\t {karyawan2.GajiBulanan}");
+            Console.WriteLine();
+            Console.WriteLine("Kenaikan gaji 10%");
+            karyawan1.AddSalaryPercentage(10);
+            karyawan2.AddSalaryPercentage(10);
+            Console.WriteLine("No  NIK/Nama \t\t\t\t Gaji Bulanan");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine($"1.  {karyawan1.NIK} {karyawan1.Nama} \t\t {karyawan1.GajiBulanan}");
+            Console.WriteLine($"1.  {karyawan2.NIK} {karyawan2.Nama} \t\t {karyawan2.GajiBulanan}");
+            Console.ReadLine();
+        }
+    }
+}
